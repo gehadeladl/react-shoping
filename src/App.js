f76d065052af44f4364276b6938493d5c45abbe4
gehadeladl <gehadeladl19@gmail.com>
2022-03-20 19:14:33 +0200

    product comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-
+import Products from './components/Products/Products';
+import data from './data'
 function App() {
+  const [product, setProduct] = useState(data)
+  console.log(product)
   return (
     <div className='layout'>
       <Header/>
-      <main>Content</main>
+      <main>
+        <div className='wrapper'>
+            <Products product={product}/>
+            <div className='filterEl'>Filter</div>
+        </div>
+      </main>
       <Footer/>
     </div>
   );
