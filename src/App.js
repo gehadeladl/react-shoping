559ee28520633783b6e1530ad10480fcc1bb787c
gehadeladl <gehadeladl19@gmail.com>
2022-03-21 14:47:19 +0200

    filter comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
+/* eslint-disable eqeqeq */
 import { useState } from 'react';
 import './App.css';
 import Filter from './components/Filter/Filter';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
-import data from './data'
+import data from './data';
 function App() {
-  const [products, setProducts] = useState(data)
+  const [products , setProducts] = useState(data) ;
+  const [size, setSize] = useState('') ;
+  const [sort, setSort] = useState('') ;
+
+  const handelFilterSize = (e) => {
+    setSize(e.target.value) ;
+
+    if(e.target.value == "ALL"){
+      setProducts(data)
+    }else{
+      let newProducts = data.filter((p) => p.sizes.indexOf(e.target.value) != -1) ;
+      setProducts(newProducts)
+    }
+  }
+
+  const handelFilterOrder = (e) => {
+    let order = e.target.value ; 
+    setSort(order)
+    let newProducts = products.sort( function(a , b){
+      if(order === "lowest"){
+        return a.price - b.price
+      }else if(order === "highest"){
+        return b.price - a.price
+      }else{
+        return a.id < b.id ? 1 : -1
+      }
+    })
+    setProducts(newProducts)
+  }
+
   return (
     <div className='layout'>
       <Header/>
       <main>
         <div className='wrapper'>
           <Products products={products}/>
-          <Filter/>
+          <Filter 
+            handelFilterSize={handelFilterSize} 
+            handelFilterOrder={handelFilterOrder}
+            size={size} 
+            sort={sort} 
+          />
         </div>
       </main>
       <Footer/>
